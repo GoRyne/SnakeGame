0c1f903ebdc6c212538303f4b6355931d97d5c74
YiJeongan <wjddks110@kookmin.ac.kr>
2022-06-03 22:00:41 +0900

    update main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ncurses.h>
+#include <unistd.h>
 #include "Map.h"
 using namespace std;
 
@@ -8,6 +9,19 @@ int main() {
     Item item;
     Stage stage;
     
-    map.InitScreen();
+    map.InitScreen(); //맵 초기화
+
+    stage = item.Generate_item(stage); //아이템 생성
+
+    stage= map.UpdateMap(stage);
+
+    map.UpdateMap(stage);
+    map.GetMission();
+    map.GetScore();
     stage = map.UpdateMap(stage);
+    map.GetScore();
+    map.GetMission();
+
+
+    return 0;
 }
